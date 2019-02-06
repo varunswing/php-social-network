d0a9b3602e8614b0518e031a3babb67ffac1c507
varunswing <9204varun@gmail.com>
2019-02-07 00:28:36 +0530

    Update contact-form.php

--- contact-form.php
+++ contact-form.php
@@ -1,29 +1,31 @@
 <?php
 
-    if(isset($_POST['contact_name']) && isset($_POST['contact_email']) && isset($_POST['contact_text'])){
-        $contact_name = $_POST['contact_name'];
-        $contact_email = $_POST['contact_email'];
-        $contact_text = $_POST['contact_text'];
-        
-        if(!empty($contact_name) && !empty($contact_email) && !empty($contact_text)){
-            if(strlen($contact_name)>25 || strlen($contact_email)>50 && strlen($contact_text)>1000){
-                echo 'Sorry, maxlength for some field has been exceeded.';
-            }else{
-                $to = '9204varun@gmail.com';
-                $subject = 'Contact form submitted';
-                $body = $contact_name."\n".$contact_text;
-                $headers = 'form:'.$contact_email;
-
-                if(@mail($to, $subject, $body, $header)){
-                    echo 'Thanks for contacting us. We\'ll be in touch soon.';
+    if(loggedin()){
+        if(isset($_POST['contact_name']) && isset($_POST['contact_email']) && isset($_POST['contact_text'])){
+            $contact_name = $_POST['contact_name'];
+            $contact_email = $_POST['contact_email'];
+            $contact_text = $_POST['contact_text'];
+            
+            if(!empty($contact_name) && !empty($contact_email) && !empty($contact_text)){
+                if(strlen($contact_name)>25 || strlen($contact_email)>50 && strlen($contact_text)>1000){
+                    echo 'Sorry, maxlength for some field has been exceeded.';
                 }else{
-                    echo '<br><br>Sorry, an error occurred. Please try again later.<br>';
+                    $to = '9204varun@gmail.com';
+                    $subject = 'Contact form submitted';
+                    $body = $contact_name."\n".$contact_text;
+                    $headers = 'form:'.$contact_email;
+
+                    if(@mail($to, $subject, $body, $header)){
+                        echo 'Thanks for contacting us. We\'ll be in touch soon.';
+                    }else{
+                        echo '<br><br>Sorry, an error occurred. Please try again later.<br>';
+                    }
                 }
             }
+        
+        }else{
+            echo '<br><br>All fields are required.'; 
         }
-    
-    }else{
-        echo '<br><br>All fields are required.'; 
     }
 
 ?>
