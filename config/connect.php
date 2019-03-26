cc0d2e5174618d7bc40454ded98c8acff2ed0c5d
varunswing <9204varun@gmail.com>
2019-03-27 00:22:43 +0530

    Delete connect.php

--- config/connect.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $HOST = 'localhost';
-    $USER = 'root';
-    $PASSWORD = '';
-    $DB = 'social-network';
-
-    $conn = mysqli_connect($HOST, $USER, $PASSWORD, $DB);
-    // if($conn) echo('DB Connected!');
-?>
