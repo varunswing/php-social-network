5dd31c8a76ef522fee35e7303e10e746175db4ff
varunswing <9204varun@gmail.com>
2019-03-27 00:21:28 +0530

    Update index.php

--- index.php
+++ index.php
@@ -23,9 +23,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <style type="text/css">
+        body {background-color:yellow;}
+    </style>
 </head>
 <body>
-
     
     <br><a href = "logout.php">logout</a>
 
