8c738db742c3c63d7c0f7abccbd47e0376315d97
varunswing <varuns939@gmail.com>
2019-08-23 23:57:27 +0530

    Update index.php

--- index.php
+++ index.php
@@ -22,14 +22,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/skeleton/2.0.4/skeleton.css" />
     <title>Document</title>
     <style type="text/css">
-        body {background-color:yellow;}
+        body {background-color:lawngreen;}
     </style>
 </head>
-<body>
-    
-    <br><a href = "logout.php">logout</a>
-
+<body link="blue" style="text-align:left">
+    <a href = "logout.php" style="text-decoration:none;">LOG OUT</a>
 </body>
 </html>
